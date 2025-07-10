4831d01ca976bea49248161d706a4b9be3547dd2
dennisyd <31513813+dennisyd@users.noreply.github.com>
2025-07-10 00:23:20 -0400

    Updated csv-merger

--- src/App.tsx
+++ src/App.tsx
@@ -243,7 +243,7 @@ function App() {
                 <Route path="tools/word-to-markdown" element={<WordToMarkdownPage />} />
                 <Route path="tools/excel-merger-splitter" element={<ExcelMergerSplitterPage />} />
                 <Route path="tools/csv-to-json" element={<CSVToJSONPage />} />
-                <Route path="tools/csv-tool-merger" element={<CSVToolMergerPage />} />
+                <Route path="tools/csv-merger" element={<CSVToolMergerPage />} />
                 <Route path="tools/color-picker" element={<ColorPickerTool />} />
                 <Route path="faq" element={<FAQ />} />
                 <Route path="terms" element={<Terms />} />
